7a46137cd88f1972923eefe2cb4f8d1f04af192f
atharvamarathe <maratheas19.comp@coep.ac.in>
2021-11-11 18:35:45 +0530

    Manage multiple torrents

---
+++ src/client.js
@@ -0,0 +1,29 @@
+const Seeder = require("./seeder");
+const Torrent = require("./torrent");
+var logger = require("log4js").getLogger("client.js");
+
+class Client {
+  constructor(options = {}) {
+    this.clientId =
+      options.clientId || "-AMVK01-" + Math.random().toString().slice(2, 14);
+    this.port = options.port || 6882;
+    this.torrents = {};
+    this.seeder = new Seeder(this.port, this);
+  }
+
+  addTorrent = (filename, options) => {
+    const t = new Torrent(filename, this.clientId, this.port, options);
+    if (!this.torrents[t.metadata.infoHash]) {
+      this.torrents[t.metadata.infoHash] = t;
+      t.start();
+      logger.info("new Torrent added and started: ", t.metadata.infoHash);
+    }
+    return t;
+  };
+
+  removeTorrent = (torrent) => {
+    delete this.torrents[torrent.metadata.infoHash];
+  };
+}
+
+module.exports = Client;
